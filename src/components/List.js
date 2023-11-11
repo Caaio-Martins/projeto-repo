f64276965a2fdf4f2ba4ebd24c90d3f5de0c85b6
Caaio-Martins <caio.antnmartins@gmail.com>
2023-11-11 12:56:27 -0300

    Avançando em Props

--- src/components/List.js
+++ src/components/List.js
@@ -6,9 +6,10 @@ const List = () => {
     <>
         <h1>Minha Lista</h1>
         <ul>
-            <Item marca="Ferrari" />
-            <Item marca="Fiat" />
+            <Item marca="Ferrari" ano_lancamento={1985} />
+            <Item marca="Fiat" ano_lancamento={1964}/>
             <Item marca="Renault" />
+            <Item marca="Chevrolet" ano_lancamento={1972}/>
         </ul>
     </>
   )
