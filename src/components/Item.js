f5e97bc6ad5fe792351e5eb837d69082b9f23e07
Caaio-Martins <caio.antnmartins@gmail.com>
2023-11-11 12:56:12 -0300

    Avançando em Props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Item = (props) => {
+const Item = ({marca, ano_lancamento}) => {
   return (
     <>
-        <li>{props.marca}</li>
-        
+        <li>{marca} - {ano_lancamento}</li>
+
     </>
   )
 }
 
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number,
+}
+
+Item.defaultProps ={
+    marca: 'Faltou a marca',
+    ano_lancamento: 0,
+}
 export default Item
