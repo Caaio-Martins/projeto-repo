493c4df54d60967d4c960908e047ff8b28684618
Caaio-Martins <caio.antnmartins@gmail.com>
2023-11-11 12:14:38 -0300

    Inserindo CSS no React (CSS modules)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
   const nome = "Maria"
 
   return (
-    <div className="App">     
-      
+    <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />     
       <SayMyName nome="Caio" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,6 +1,10 @@
+import styles from './Frase.module.css'
+ 
 function Frase(){
     return (
         <div>
+            <div className={styles.fraseContainer}></div>
+            <p className={styles.fraseContent}></p>
             <p>Este é um componente com uma frase!</p>
         </div>
     )
